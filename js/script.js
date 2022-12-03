22b1b0a0553ebf2a98aff60ea63ce804178d9deb
kson4 <davidsinternetmail@gmail.com>
2022-12-03 14:02:17 -0800

    skills

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 const clientY = document.documentElement.clientHeight;
 const hero = document.querySelector("#home .text");
 window.addEventListener("scroll", () => {
-  hero.style.background = `rgba(0, 0, 0,${window.pageYOffset / clientY - 0.1})`;
+  hero.style.background = `rgba(0, 0, 0, ${
+    window.pageYOffset / clientY - 0.1
+  })`;
 });
 const about = document.querySelector("#about .text");
 window.addEventListener("scroll", () => {
-  about.style.background = `rgba(0, 0, 0,${1 - window.pageYOffset / clientY})`;
+  about.style.background = `rgba(0, 0, 0, ${1 - window.pageYOffset / clientY})`;
+  about.style.color = `rgba(255, 255, 255, ${window.pageYOffset / clientY})`;
 });
