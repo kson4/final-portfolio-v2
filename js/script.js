5e18a2bffa4c0a40c97a3b5b9b18d01cf06a4931
kson4 <davidsinternetmail@gmail.com>
2022-12-03 13:03:13 -0800

    update hero page, completed about page

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const clientY = document.documentElement.clientHeight;
+const hero = document.querySelector("#home .text");
+window.addEventListener("scroll", () => {
+  hero.style.background = `rgba(0, 0, 0,${window.pageYOffset / clientY - 0.1})`;
+});
+const about = document.querySelector("#about .text");
+window.addEventListener("scroll", () => {
+  about.style.background = `rgba(0, 0, 0,${1 - window.pageYOffset / clientY})`;
+});
