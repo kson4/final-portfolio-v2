57d985abccb01a0d0e3b0282fd166c75a7a380fe
kson4 <davidsinternetmail@gmail.com>
2022-12-05 09:01:21 -0800

    skills page and scroll effect

--- js/script.js
+++ js/script.js
@@ -6,7 +6,38 @@ window.addEventListener("scroll", () => {
   })`;
 });
 const about = document.querySelector("#about .text");
+const aboutP = document.querySelectorAll(".about-text-p");
 window.addEventListener("scroll", () => {
   about.style.background = `rgba(0, 0, 0, ${1 - window.pageYOffset / clientY})`;
   about.style.color = `rgba(255, 255, 255, ${window.pageYOffset / clientY})`;
+  if (window.pageYOffset / clientY > 1) {
+    about.style.background = `rgba(0, 0, 0, ${
+      window.pageYOffset / clientY - 1
+    })`;
+    aboutP.forEach((item) => {
+      item.style.color = `rgba(255,255,255, ${
+        2 - window.pageYOffset / clientY
+      })`;
+    });
+  }
+});
+const skillsP = document.querySelector("#skills");
+const skillsH = document.querySelector("#skills h2");
+const skillsTxt = document.querySelector("#skills .flex");
+window.addEventListener("scroll", () => {
+  skillsH.style.background = `rgba(0, 0, 0, ${
+    2.2 - window.pageYOffset / clientY
+  })`;
+  skillsTxt.style.background = `rgba(0, 0, 0, ${
+    2.2 - window.pageYOffset / clientY
+  })`;
+  if (window.pageYOffset / clientY > 2) {
+    skillsTxt.style.background = `rgba(0, 0, 0, ${
+      window.pageYOffset / clientY - 2
+    })`;
+    skillsTxt.style.color = `rgba(255,255,255, ${
+      3 - window.pageYOffset / clientY
+    })`;
+  }
+  console.log(window.pageYOffset / clientY);
 });
